20bda1bce8057dd9488ff09499dd77f22209858f
SanSjo <susanna.sjoberg@gmail.com>
2020-04-24 13:34:23 +0200

    made chnages in megamenu

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,103 +1,34 @@
 import React from 'react';
 import './styles/mega-menu.css';
+import Dropdown, {
+	DropdownToggle,
+	DropdownMenu,
+	DropdownMenuWrapper,
+	MenuItem,
+	DropdownButton
+} from '@trendmicro/react-dropdown';
 import ProductMenu from './ProductMenu';
 
 export const Categories = () => {
 	return (
-		<div className="dropdown">
-			<button type="button" className=" dropbtn nav-button">
-				KATEGORIER <i className="fa fa-caret-down" />
-			</button>
-			<div className="dropdown-content">
-				<div className="row">
-					<ProductMenu
-						title="A"
-						product1="Abonnemang - MetaTrak"
-						link1=""
-						product2="Anafi - Drönare"
-						link2=""
-						product3="Antenn"
-						link3=""
-						product4="Antennförstärkare"
-						link4=""
-						product5="Arbetsljus LED - LAP"
-						link5=""
-						product6="Aux kablage"
-						link6=""
-						product7="Aux universal kablage"
-						link7=""
-						product8="Avståndsvarnare"
-						link8=""
-						product9=""
-						link9=""
-						product10=""
-						link10=""
-					/>
-					<ProductMenu
-						title="B"
-						product1="Backsensor"
-						link1=""
-						product2="Backkamera 360° sats"
-						link2=""
-						product3="Back-TV"
-						link3=""
-						product4="Backvarnare"
-						link4=""
-						product5="Bagagestroppar"
-						link5=""
-						product6="Basplatta - Carcomm"
-						link6=""
-						product7="Batterikabelskor"
-						link7=""
-						product8="Bebop-Pro"
-						link8=""
-						product9="Bistabila - Huvudströmbrytare"
-						link9=""
-						product10="Bilstereo"
-						link10=""
-						product11="Blinkljus LED"
-						link11=""
-						product12="Bluetooth"
-						link11=""
-						product13="Båtlarm"
-						link11=""
-					/>
-					<ProductMenu
-						title="C"
-						product1="Canbus - Farthållare"
-						link1=""
-						product2="Canbus - Fordonslarm"
-						link2=""
-						product3="Carcomm"
-						link3=""
-						product4="Cobra"
-						link4=""
-						product5="Bagagestroppar"
-						link5=""
-						product6="Basplatta - Carcomm"
-						link6=""
-						product7="Batterikabelskor"
-						link7=""
-						product8="Bebop-Pro"
-						link8=""
-						product9="Bistabila - Huvudströmbrytare"
-						link9=""
-						product10="Bilstereo"
-						link10=""
-						product11="Blinkljus LED"
-						link11=""
-						product12="Bluetooth"
-						link11=""
-						product13="Båtlarm"
-						link11=""
-					/>
-					Canbus - Farthållare Canbus - Fordonslarm Canbus - Interface Carcomm -
-					Aktiva hållare Carcomm - Antenner Carcomm - Fästen Carcomm - Musik
-					hållare Carcomm - PDA hållare Carcomm - Svanhalsmikrofon Centrallås -
-					Pulsrelä - Datalinker Cobra - Fordonslarm
-				</div>
-			</div>
-		</div>
+		<Dropdown>
+			<Dropdown.Toggle title="Select an option" />
+			<Dropdown.Menu>
+				<MenuItem>Menu item one</MenuItem>
+				<MenuItem>Menu item two</MenuItem>
+				<MenuItem>Menu item three</MenuItem>
+				<MenuItem divider />
+				<MenuItem>
+					Menu item four
+					<MenuItem>Second level item one</MenuItem>
+					<MenuItem>Second level item two</MenuItem>
+					<MenuItem>
+						Second level item three
+						<MenuItem>Third level item one</MenuItem>
+					</MenuItem>
+				</MenuItem>
+			</Dropdown.Menu>
+		</Dropdown>
 	);
 };
 
@@ -109,24 +40,297 @@ export default Categories;
 // Antenn - TV
 // Antenn - TV 24V
 
-/* <div className="column">
+// <ProductMenu
+// 	title="A"
+// 	product1="Abonnemang - MetaTrak"
+// 	link1=""
+// 	product2="Anafi - Drönare"
+// 	link2=""
+// 	product3="Antenn"
+// 	link3=""
+// 	product4="Antennförstärkare"
+// 	link4=""
+// 	product5="Arbetsljus LED - LAP"
+// 	link5=""
+// 	product6="Aux kablage"
+// 	link6=""
+// 	product7="Aux universal kablage"
+// 	link7=""
+// 	product8="Avståndsvarnare"
+// 	link8=""
+// 	product9=""
+// 	link9=""
+// 	product10=""
+// 	link10=""
+// />
+// <ProductMenu
+// 	title="B"
+// 	product1="Backsensor"
+// 	link1=""
+// 	product2="Backkamera 360° sats"
+// 	link2=""
+// 	product3="Back-TV"
+// 	link3=""
+// 	product4="Backvarnare"
+// 	link4=""
+// 	product5="Bagagestroppar"
+// 	link5=""
+// 	product6="Basplatta - Carcomm"
+// 	link6=""
+// 	product7="Batterikabelskor"
+// 	link7=""
+// 	product8="Bebop-Pro"
+// 	link8=""
+// 	product9="Bistabila - Huvudströmbrytare"
+// 	link9=""
+// 	product10="Bilstereo"
+// 	link10=""
+// 	product11="Blinkljus LED"
+// 	link11=""
+// 	product12="Bluetooth"
+// 	link11=""
+// 	product13="Båtlarm"
+// 	link11=""
+// />
+// <ProductMenu
+// 	title="C"
+// 	product1="Canbus - Farthållare"
+// 	link1=""
+// 	product2="Canbus - Fordonslarm"
+// 	link2=""
+// 	product3="Carcomm - antenner, fästen, Musik, PDA-hållare, Svanhalsmikrofon"
+// 	link3=""
+// 	product4="Centrallås - Pulsrelä"
+// 	link4=""
+// 	product5="Cobra - Fordonslarm"
+// 	link5=""
+// />
+// <ProductMenu
+// 	title="D"
+// 	product1="DASH2 Carcomm - Aktiva hållare"
+// 	link1=""
+// 	product2="Datalinker - Cobra"
+// 	link2=""
+// 	product3="DC/DC omvandlare"
+// 	link3=""
+// 	product4="Diod"
+// 	link4=""
+// 	product5="Disco FPV - Drönare"
+// 	link5=""
+// 	product6="Drönare - Parrot"
+// 	link6=""
+// 	product7="DVD - 24V, Bilstereo"
+// 	link7=""
+// 	product8="DVR Back-TV system"
+// 	link8=""
+// 	product9="Dynamo med vev - Nödapparat"
+// 	link9=""
+// />
+// <ProductMenu title="E" product1="Elektronisk P-skiva" link1="" />
+// <ProductMenu
+// 	title="F"
+// 	product1="Farthållare"
+// 	link1=""
+// 	product2="Fjärrstyrning"
+// 	link2=""
+// 	product3="FLIR värmekamera - Drönare"
+// 	link3=""
+// 	product4="FM Modulator"
+// 	link4=""
+// 	product5="FM/AM radio - Nödapparat"
+// 	link5=""
+// 	product6="Fordonsbelysning LED -LAP"
+// 	link6=""
+// 	product7="Fordonslarm - Cobra, Meta, Texalarm"
+// 	link7=""
+// 	product8="Funkwerk antennförstärkare, Handsfree"
+// 	link8=""
+// />
+// <ProductMenu
+// 	title="G"
+// 	product1="Gaslarm"
+// 	link1=""
+// 	product2="Gassensor"
+// 	link2=""
+// 	product3="GPS - larm, stöldskydd/söksystem, tracker"
+// 	link3=""
+// 	product4="GSM"
+// 	link4=""
+// 	product5="GSM - larm, telefon"
+// 	link5=""
+// 	product6="Guidesystem - Dator/Ljudsystem, Förstärkare/Mixer, Hörlur, Mikrofon"
+// 	link6=""
+// />
+// <ProductMenu
+// 	title="H"
+// 	product1="Gaslarm"
+// 	link1=""
+// 	product2="Gassensor"
+// 	link2=""
+// 	product3="GPS - larm, stöldskydd/söksystem, tracker"
+// 	link3=""
+// 	product4="GSM"
+// 	link4=""
+// 	product5="GSM - larm, telefon"
+// 	link5=""
+// 	product6="Guidesystem - Dator/Ljudsystem, Förstärkare/Mixer, Hörlur, Mikrofon"
+// 	link6=""
+// />
+// <ProductMenu
+// 	title="I"
+// 	product1="Gaslarm"
+// 	link1=""
+// 	product2="Gassensor"
+// 	link2=""
+// 	product3="GPS - larm, stöldskydd/söksystem, tracker"
+// 	link3=""
+// 	product4="GSM"
+// 	link4=""
+// 	product5="GSM - larm, telefon"
+// 	link5=""
+// 	product6="Guidesystem - Dator/Ljudsystem, Förstärkare/Mixer, Hörlur, Mikrofon"
+// 	link6=""
+// />
+// <ProductMenu
+// 	title="J"
+// 	product1="Gaslarm"
+// 	link1=""
+// 	product2="Gassensor"
+// 	link2=""
+// 	product3="GPS - larm, stöldskydd/söksystem, tracker"
+// 	link3=""
+// 	product4="GSM"
+// 	link4=""
+// 	product5="GSM - larm, telefon"
+// 	link5=""
+// 	product6="Guidesystem - Dator/Ljudsystem, Förstärkare/Mixer, Hörlur, Mikrofon"
+// 	link6=""
+// />
+// <ProductMenu
+// 	title="K"
+// 	product1="Gaslarm"
+// 	link1=""
+// 	product2="Gassensor"
+// 	link2=""
+// 	product3="GPS - larm, stöldskydd/söksystem, tracker"
+// 	link3=""
+// 	product4="GSM"
+// 	link4=""
+// 	product5="GSM - larm, telefon"
+// 	link5=""
+// 	product6="Guidesystem - Dator/Ljudsystem, Förstärkare/Mixer, Hörlur, Mikrofon"
+// 	link6=""
+// />
+// <ProductMenu
+// 	title="L"
+// 	product1="Gaslarm"
+// 	link1=""
+// 	product2="Gassensor"
+// 	link2=""
+// 	product3="GPS - larm, stöldskydd/söksystem, tracker"
+// 	link3=""
+// 	product4="GSM"
+// 	link4=""
+// 	product5="GSM - larm, telefon"
+// 	link5=""
+// 	product6="Guidesystem - Dator/Ljudsystem, Förstärkare/Mixer, Hörlur, Mikrofon"
+// 	link6=""
+// />
+// <ProductMenu
+// 	title="M"
+// 	product1="Gaslarm"
+// 	link1=""
+// 	product2="Gassensor"
+// 	link2=""
+// 	product3="GPS - larm, stöldskydd/söksystem, tracker"
+// 	link3=""
+// 	product4="GSM"
+// 	link4=""
+// 	product5="GSM - larm, telefon"
+// 	link5=""
+// 	product6="Guidesystem - Dator/Ljudsystem, Förstärkare/Mixer, Hörlur, Mikrofon"
+// 	link6=""
+// />
+// <ProductMenu
+// 	title="N"
+// 	product1="Gaslarm"
+// 	link1=""
+// 	product2="Gassensor"
+// 	link2=""
+// 	product3="GPS - larm, stöldskydd/söksystem, tracker"
+// 	link3=""
+// 	product4="GSM"
+// 	link4=""
+// 	product5="GSM - larm, telefon"
+// 	link5=""
+// 	product6="Guidesystem - Dator/Ljudsystem, Förstärkare/Mixer, Hörlur, Mikrofon"
+// 	link6=""
+// />
+// <ProductMenu
+// 	title="P"
+// 	product1="Gaslarm"
+// 	link1=""
+// 	product2="Gassensor"
+// 	link2=""
+// 	product3="GPS - larm, stöldskydd/söksystem, tracker"
+// 	link3=""
+// 	product4="GSM"
+// 	link4=""
+// 	product5="GSM - larm, telefon"
+// 	link5=""
+// 	product6="Guidesystem - Dator/Ljudsystem, Förstärkare/Mixer, Hörlur, Mikrofon"
+// 	link6=""
+// />
+// <ProductMenu
+// 	title="Q"
+// 	product1="Gaslarm"
+// 	link1=""
+// 	product2="Gassensor"
+// 	link2=""
+// 	product3="GPS - larm, stöldskydd/söksystem, tracker"
+// 	link3=""
+// 	product4="GSM"
+// 	link4=""
+// 	product5="GSM - larm, telefon"
+// 	link5=""
+// 	product6="Guidesystem - Dator/Ljudsystem, Förstärkare/Mixer, Hörlur, Mikrofon"
+// 	link6=""
+// />
+
+/*
+<div className="dropdown">
+			<button type="button" className=" dropbtn nav-button">
+				KATEGORIER <i className="fa fa-caret-down" />
+			</button>
+			<div className="dropdown-content">
+				<div className="row">
+					<div className="column">
 						<h3>Bluetooth & Headset</h3>
-						<a href="#">
-							<i className="fa fa-angle-right" aria-hidden="true" /> Sena
-							Headset
-						</a>
-						<a href="#">
+						<div>
 							<i className="fa fa-angle-right" aria-hidden="true" />{' '}
-							OnCarDisplay Handsfree
-						</a>
-						<a href="#">
-							<i className="fa fa-angle-right" aria-hidden="true" /> Parrot
-							Handsfree
-						</a>
-						<a href="#">
+							<p>Sena Headset</p>
+						</div>
+						<div>
+							<i className="fa fa-angle-right" aria-hidden="true" />{' '}
+							<p> OnCarDisplay Handsfree</p>
+						</div>
+						<div className="dropbtn nav-button">
+							<i className="fa fa-angle-right" aria-hidden="true" />{' '}
+							<p>Parrot Handsfree</p>
+							<div className="dropdown-content">
+								<div>
+									<div>
+										<div>
+											<p>Parrot</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<p>
 							<i className="fa fa-angle-right" aria-hidden="true" /> Handsfree
 							MUTE kablar
-						</a>
+						</p>
 						<hr />
 						<a href="#">
 							<i className="fa fa-angle-right" aria-hidden="true" /> Parrot Zik
@@ -416,4 +620,8 @@ export default Categories;
 						<a href="#">Link 1</a>
 						<a href="#">Link 2</a>
 						<a href="#">Link 3</a>
-					</div> */
+					</div>
+				</div>
+			</div>
+		</div>
+		*/
--- src/components/ProductMenu.js
+++ src/components/ProductMenu.js
@@ -1,48 +1,49 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './styles/mega-menu.css';
 
 export const ProductMenu = props => {
 	return (
 		<div className="column">
-			<h3>{props.title}</h3>
+			<h3 className="productmenu-header">{props.title}</h3>
 			<Link to={props.link1}>
-				<p>{props.product1}</p>{' '}
+				<p className="product-text">{props.product1}</p>{' '}
 			</Link>
 			<Link to={props.link2}>
-				<p>{props.product2}</p>
+				<p className="product-text">{props.product2}</p>
 			</Link>
 			<Link to={props.link3}>
-				<p>{props.product3}</p>
+				<p className="product-text">{props.product3}</p>
 			</Link>
 			<Link to={props.link4}>
-				<p>{props.product4}</p>
+				<p className="product-text">{props.product4}</p>
 			</Link>
 			<Link to={props.link5}>
-				<p>{props.product5}</p>
+				<p className="product-text">{props.product5}</p>
 			</Link>
 			<Link to={props.link6}>
-				<p>{props.product6}</p>
+				<p className="product-text">{props.product6}</p>
 			</Link>
 			<Link to={props.link7}>
-				<p>{props.product7}</p>
+				<p className="product-text">{props.product7}</p>
 			</Link>
 			<Link to={props.link8}>
-				<p>{props.product8}</p>
+				<p className="product-text">{props.product8}</p>
 			</Link>
 			<Link to={props.link9}>
-				<p>{props.product9}</p>
+				<p className="product-text">{props.product9}</p>
 			</Link>
 			<Link to={props.link10}>
-				<p>{props.product10}</p>
+				<p className="product-text">{props.product10}</p>
 			</Link>
 			<Link to={props.link11}>
-				<p>{props.product11}</p>
+				<p className="product-text">{props.product11}</p>
 			</Link>
 			<Link to={props.link12}>
-				<p>{props.product12}</p>
+				<p className="product-text">{props.product12}</p>
 			</Link>
 			<Link to={props.link13}>
-				<p>{props.product13}</p>
+				<p className="product-text">{props.product13}</p>
 			</Link>
 		</div>
 	);
